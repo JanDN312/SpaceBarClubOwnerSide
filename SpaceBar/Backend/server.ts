622878000b16eda2130d0c49c668ecab641a0fde
JanDN312 <J.Dario@gmx.de>
2022-01-21 23:48:37 +0100

    update 3

--- SpaceBar/Backend/server.ts
+++ SpaceBar/Backend/server.ts
@@ -2,15 +2,17 @@ import {Datareader} from "./datafile.ts"
 
 const listener = Deno.listen({port: 5000});
 const dataloader = new Datareader()
+
 async function getResponse( reqinput: Request){
-    console.log(reqinput)//reqinput muss als input ein dataset sein mit aufbau: {artDesZugriffs(w/r),[Schreibelement/Lesekriterien]}
-    //TODO Abfrage des Schreibzugriffs
-    //TODO Suchfunktion in der Datenbank
-    return reqinput;}
+  //reqinput muss als input ein dataset sein mit aufbau: {artDesZugriffs(w/r),[Schreibelement/Lesekriterien]}
+  //TODO Abfrage des Schreibzugriffs
+  console.log(JSON.stringify(reqinput.text))
+  
+  return dataloader.readDataElement(await reqinput.text())}
 
 for await(const conn of listener)
   handleNewConnection(conn);async function handleNewConnection(conn: Deno.Conn) {
   for await(const {request:req, respondWith:res} of Deno.serveHttp(conn)) {
-    res(new Response((await getResponse(req)).body));
+    res(new Response((await getResponse(req))));
   }
 }

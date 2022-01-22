604bf1587bcba030c67eb484bfdfb660a812ca4e
mimed100 <michel.medved@gmail.com>
2022-01-22 18:17:03 +0100

    Docer nicht notwendig mit readTextFileSync

--- read_write_JSON_in_deno/mod.ts
+++ read_write_JSON_in_deno/mod.ts
@@ -1,9 +1,10 @@
 const path = './fruits.json';
 
 let data;
-const decoder = new TextDecoder('utf-8')
+//const decoder = new TextDecoder('utf-8')
+//data = JSON.parse(decoder.decode(Deno.readFileSync(path)));
 
-data = JSON.parse(decoder.decode(Deno.readFileSync(path)));
+data = JSON.parse(Deno.readTextFileSync(path))
 
 console.log(typeof data);
 console.log(data);

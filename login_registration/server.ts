e770efbbe669932c0834a1fbea3f3dc4f3f4abb0
mimed100 <michel.medved@gmail.com>
2022-01-23 22:26:02 +0100

    Error... source code could not be parsed

--- login_registration/server.ts
+++ login_registration/server.ts
@@ -1,12 +1,26 @@
 import {Application, Router} from "https://deno.land/x/oak@v9.0.0/mod.ts";
-import {home} from "./routes.ts";
+import {home, login, postLogin, register, postRegister, logout, protectedRoute,} from "./routes.ts";
 
 const app = new Application();
 
 const router = new Router();
-router.get('/', home);
+
+router
+    .get('/', home)
+    .get('/login', login)
+    .get('/login', postLogin)
+    .get('/register', register)
+    .get('/register', postRegister)
+    .get('/logout', logout)
+    .get('/protected', protectedRoute)
+    ;
+
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+app.addEventListener('error', evt => {
+    console.log(evt.error);
+})
 
 app.listen({port: 8000});
 console.log("Server started on port: 8000")

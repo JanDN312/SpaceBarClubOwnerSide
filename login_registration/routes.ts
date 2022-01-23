c8d5369f05b10c573992d74013d4f7634f6c3bcf
mimed100 <michel.medved@gmail.com>
2022-01-23 22:26:24 +0100

    Error: source code can not be passed

--- login_registration/routes.ts
+++ login_registration/routes.ts
@@ -4,3 +4,27 @@ import {renderFileToString} from "https://deno.land/x/dejs@0.10.2/mod.ts";
 export const home = async (ctx: RouterContext) => {
     ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/home.ejs`, {});
 }
+
+export const login = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/login.ejs`, {});
+}
+
+export const postLogin = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/home.ejs`, {});
+}
+
+export const register = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/register.ejs`, {});
+}
+
+export const postRegister = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/home.ejs`, {});
+}
+
+export const logout = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/home.ejs`, {});
+}
+
+export const protectedRoute = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/protected.ejs`, {});
+}

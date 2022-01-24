2b2554d1491dde0a6d01080d4532f831e8937dea
Coreprog <97054577+Coreprog@users.noreply.github.com>
2022-01-24 22:06:36 +0100

    Svelte Bewertungsseite

---
+++ SpaceBar/Frontend/App.svelte
@@ -0,0 +1,44 @@
+<script>
+import FeedbackList from "./components/feedbacklist.svelte"
+import FeedbackStats from "./components/feedbackstats.svelte"
+import FeedbackForm from "./components/feedbackform.svelte"
+
+let feedback = [
+	{
+		id: 1,
+		rating:10,
+		text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+	},
+	{
+		id: 2,
+		rating:9,
+		text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+	},
+	{
+		id: 3,
+		rating:8,
+		text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+	}
+]
+
+$: count = feedback.length
+$: average = feedback.reduce((a,{rating})=>a + rating, 0)/feedback.length
+
+const addFeedback =(e) => {
+	const newFed = e.detail
+	feedback = [newFed, ...feedback]
+}
+
+const deleteFeedback = (e) => {
+	const Itemid = e.detail
+	feedback = feedback.filter((item)=> item.id != Itemid)
+}
+
+
+</script>
+
+<main class="container">
+	<FeedbackForm on:add-feedback={addFeedback} />
+	<FeedbackStats {count} {average} />
+	<FeedbackList feedback={feedback} on:delete-feedback={deleteFeedback} />
+</main>

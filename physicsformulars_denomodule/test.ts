fee041d100df026ca0c47564a65c7a813fc2ec8e
JanDN312 <J.Dario@gmx.de>
2022-01-26 18:35:53 +0100

    v3.2.1

--- physicsformulars_denomodule/test.ts
+++ physicsformulars_denomodule/test.ts
@@ -1,11 +1,11 @@
 import {AcceleratedMovementFormulars as amf} from "./src/acceleratedmovements.ts"
 
-var distance= 1000        //in meter
-var acceleration= 20    //in m/s^2
-var time= 10           //in s
+var distance= 1000        
+var acceleration= 20   
+var time= 10           
 
-console.log("Accelerated Motion")
 console.log(" ")
+console.log("Accelerated Motion")
 console.log("Acceleration:")
 console.log(amf.getAccelerationDAT_AM(distance, time))
 console.log("Distance:")
@@ -20,8 +20,8 @@ var power=20
 var mass=10
 var acceleration=2
 
-console.log("General Power Formular")
 console.log(" ")
+console.log("General Power Formular")
 console.log("Acceleration:")
 console.log(pm.getAccelerationPMA_FF(power, mass))
 console.log("Mass:")
@@ -36,8 +36,8 @@ var time=10
 var velocity=10
 var distance=100
 
-console.log("Steady Motion")
 console.log(" ")
+console.log("Steady Motion")
 console.log("Acceleration")
 console.log(smf.getDistanceDVT_SM(velocity, time))
 console.log("Time")
@@ -52,8 +52,8 @@ var energy= 300
 var height= 14
 var mass= 12
 
-console.log("Potential Energy")
 console.log(" ")
+console.log("Potential Energy")
 console.log("Energy:")
 console.log(ef.getEnergyEMH_PotentialEnergy(mass, height))
 console.log("Height")

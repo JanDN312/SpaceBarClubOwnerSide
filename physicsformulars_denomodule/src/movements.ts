d59549027f8008eec1bdef7f81cf6399dc7f06e6
JanDN312 <J.Dario@gmx.de>
2022-01-26 12:26:11 +0100

    v1.0.1

--- physicsformulars_denomodule/src/movements.ts
+++ physicsformulars_denomodule/src/movements.ts
@@ -1,26 +1,26 @@
 export class MovementFormulars{
-    //velocity=acceleration*time SI(m,s)
-    public static getAccelerationVAT(velocity:number, time:number) {
+    //velocity=acceleration*time SI(m,s) VAT_SM Velocity,Acceleration,Time _ Steady Motion
+    public static getAccelerationVAT_SM(velocity:number, time:number) {
     return velocity/time}
-    public static getVelocityVAT(acceleration:number, time:number){
+    public static getVelocityVAT_SM(acceleration:number, time:number){
     return acceleration*time}
-    public static getTimeVAT(velocity:number, acceleration:number){
+    public static getTimeVAT_SM(velocity:number, acceleration:number){
     return velocity/acceleration}
 
-    //distance=velocity*time SI(m,s)
-    public static getDistanceDVT(velocity:number,time:number){
+    //distance=velocity*time SI(m,s)  DVT_SM Distanve,Velocity,Time _ Steady Motion
+    public static getDistanceDVT_SM(velocity:number,time:number){
         return velocity*time}
-    public static getVelocityDVT(distance:number,time:number){
+    public static getVelocityDVT_SM(distance:number,time:number){
         return distance/time}
-    public static getTimeDVT(distance:number,velocity:number){
+    public static getTimeDVT_SM(distance:number,velocity:number){
         return distance/velocity}
 
-    //distance=0,5*acceleration*time SI(m,s)
-    public static getDistanceDAT(acceleration:number, time:number){
+    //distance=0,5*acceleration*time SI(m,s) DAT_AM Distanve,Acceleration,Time _ Accelerated Motion
+    public static getDistanceDAT_AM(acceleration:number, time:number){
         return 0.5*acceleration*time}
-    public static getAccelerationDAT(distance:number,time:number){
+    public static getAccelerationDAT_AM(distance:number,time:number){
         return 2*distance/time}
-    public static getTimeDAT(distance:number, acceleration:number){
+    public static getTimeDAT_AM(distance:number, acceleration:number){
         return 2*distance/acceleration}
     
 }

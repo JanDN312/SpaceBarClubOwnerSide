087cf1c4b286eaade60b6ab2d80fa4451d6aa717
JanDN312 <J.Dario@gmx.de>
2022-01-26 12:35:56 +0100

    v1.1.1

--- physicsformulars_denomodule/src/movements.ts
+++ physicsformulars_denomodule/src/movements.ts
@@ -15,12 +15,20 @@ export class MovementFormulars{
     public static getTimeDVT_SM(distance:number,velocity:number){
         return distance/velocity}
 
-    //distance=0,5*acceleration*time SI(m,s) DAT_AM Distanve,Acceleration,Time _ Accelerated Motion
+    //distance=0,5*acceleration*time SI(m,s) DAT_AM Distance,Acceleration,Time _ Accelerated Motion
     public static getDistanceDAT_AM(acceleration:number, time:number){
         return 0.5*acceleration*time}
     public static getAccelerationDAT_AM(distance:number,time:number){
         return 2*distance/time}
     public static getTimeDAT_AM(distance:number, acceleration:number){
         return 2*distance/acceleration}
+
+    //distance=(velocity^2)/(2*acceleration) SI(m,s) DVA_AM Distance,Velocity,Acceleration _ Accelerated Motion
+    public static getDistanceDVA_AM(velocity:number, acceleration:number){
+        return (velocity**2)/(2*acceleration)}
+    public static getVelocityDVA_AM(distance:number, acceleration:number){
+        return (2*distance*acceleration)**(0.5)}
+    public static getAccelerationDVA_AM(distance:number, velocity:number){
+        return(velocity**2)/(2*distance)}
     
 }
